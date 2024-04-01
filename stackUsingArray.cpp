b5885da3e47f40013ab67a90b3c06fad43ec4b4f
Om Raja <148458161+Om-Raja@users.noreply.github.com>
2024-04-01 12:42:45 +0000

    added function of checking palindrome. But code is not working (cry)

--- stackUsingArray.cpp
+++ stackUsingArray.cpp
@@ -1,17 +1,23 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 //Practical 2 - Stack using Array
-class stackk {
+class stack {
     public:
-    int array[MAX];
     int MAX = 100;
+    int array[MAX];
     int header = -1;
 
-    void push(int newElement)
+    void push()
     {
         if (header == MAX)
         {
             cout << "Overflow";
         }
         else {
+            int newElement;
+            cout << "Enter the new element : ";
+            cin >> newElement;
             array[++header] = newElement;
         }
     }
@@ -34,12 +40,61 @@ class stackk {
         }
     }
 
+    void checkPalindrome()
+    {
+        char string1[MAX];
+        int sizeOfString;
+        cout << "Enter the size of string";
+        cin >> sizeOfString;
+        pointer = 0;
+        char newChar;
+        cout << "Enter the character by sequence, press ENTER after every character : ";
+        for(int i = 0; i < sizeOfString; i++)
+        {
+            cin >> newChar;
+            string1[pointer] = newChar;
+            pointer++;
+        }
+
+        char string2[sizeOfString];
+        for(int i = 0; i <= sizeOfString; i++)
+        {
+            string2[i] = string1[pointer];
+            pointer--;
+        }
+        if(string1 === string2)
+            cout << "It's a Palindrome.";
+        else
+            cout << "It's not a palindrome.";
+
+    }
+
 };
 
 int main()
 {
     while(true){
         cout << "Choose the operation : \n";
-        cout << "1. Push \n"
+        cout << "1. Push \n2. Pop \n3. Show \n4. Check Palindrome \n5. Exit";
+        int option;
+        cin >> option;
+        switch(option)
+        {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                show();
+                break;
+            case 4: 
+                checkPalindrome();
+            case 5: 
+                exit(0);
+
+        }
+        
     }
 }
